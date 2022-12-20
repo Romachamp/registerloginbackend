e1e67f3f66a2386103480c99465920662b68351a
roma <palchyk.roman@gmail.com>
2022-12-20 21:59:46 +0200

    por fixed

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const app = express();
 
 const main_route = require('./routes/main.route');
 
+const PORT = process.env.PORT || 3030;
+
 app.use('/api', main_route);
 
-app.listen(443, function (e) {
+app.listen(PORT, function (e) {
     console.log('server started');
 });
