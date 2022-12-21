047da85f43868e74a1863370d7a330acf7f622e4
roma <palchyk.roman@gmail.com>
2022-12-21 09:28:29 +0200

    por fixed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const main_route = require('./routes/main.route');
 
+const cors=require("cors");
+const corsOptions ={
+    origin:'*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 const PORT = process.env.PORT || 3030;
 
 app.use('/api', main_route);
